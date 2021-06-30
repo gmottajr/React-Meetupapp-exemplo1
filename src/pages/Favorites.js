3b6ca718726e01f9b9803bfb8da965214214a3a5
Gerson <jrmottagerson@gmail.com>
2021-06-30 03:55:19 -0300

    Favorites context and page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,22 @@
+import {useContext} from 'react';
+import FavoritesContext from '../store/favorites-context';
+import MeetupList from '../components/meetups/MeetupList';
+
 function FavoritesPage() {
+    const favoritesCtx = useContext(FavoritesContext);
+    let auxContent;
+
+    if (favoritesCtx.totalFavorites === 0)
+    {
+        auxContent = <p>You got no favorites yet. Start adding some?</p>
+    } else {
+        auxContent = <MeetupList meetups={favoritesCtx.favorites}/>;
+    }
+
     return (
         <div>
             <h1>My Favorites</h1>
+            {auxContent}
         </div>
     );
 }
