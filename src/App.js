11af2f614fb6af4b87d8025a65acd89020e733dd
Gerson <jrmottagerson@gmail.com>
2021-06-29 22:04:07 -0300

    Route and  pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {Route, Switch} from 'react-router-dom'; 
+import AllMeetupsPage from './pages/AllMettups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
 
 function App() {
   return (
     <div>
-      Nada aqui .... starting ...
+      <Switch>
+        <Route path='/' exact={true}>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+
     </div>
   );
 }
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,9 @@
+function NewMeetupPage() {
+    return (
+        <div>
+            <h1>Add new meetup</h1>
+        </div>
+    );
+}
+
+export default NewMeetupPage;
